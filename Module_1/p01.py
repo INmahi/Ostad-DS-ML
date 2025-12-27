1a353a9e3f86578ae3904829d086fbf8e24f584a
Ishat Noor Mahi <ishatnoormahi787@gmail.com>
2025-12-27 17:15:29 +0600

    assignments

--- Module_1/p01.py
+++ Module_1/p01.py
@@ -1,11 +1,18 @@
 
-#PROBLEM 1: Write a Python program that takes a number as input and checks whether it is even or odd.
+# PROBLEM 1: Write a Python program that takes a number as input and checks whether it is even or odd.
 
-num = int(input("Enter A Number: "))
+# num = int(input("Enter A Number: "))
 
-if num%2==0:
-    print("Even")
-else:
-    print("Odd")
+# if num%2==0:
+#     print("Even")
+# else:
+#     print("Odd")
 
 
+
+cse_marks = input("Enter your mark in CSE: ")
+acc_marks = input("Enter your mark in Accounting: ")
+
+# print(f"Your total marks: {cse_marks+acc_marks}")
+
+print(f"cse percentage: {(cse_marks/80)*100}%")
